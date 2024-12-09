ddf69d4f142f320a028b1e83a8e14555aaff004e
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-09 20:10:17 +0100

    更新测试文件

--- python_script/客户端测试.py
+++ python_script/客户端测试.py
@@ -1,50 +1,41 @@
 import asyncio
 from sys_basis.WebSocket_Client import WebSocketClient
-from ocpp.v201 import ChargePoint, call
-from datetime import datetime
+from ocpp.v201 import ChargePoint
 from sys_basis.Generator_Ocpp_Std.V2_0_1 import *
 
 
-# 自定义ChargePoint类, 发送 BootNotification 消息
 class MyChargePoint(ChargePoint):
     async def send_boot_notification(self):
-        boot_notification = authorize_request.generate(id_token=authorize_request.get_id_tocken('ddd', type='Central'))
-
-        print('Sending BootNotification:', boot_notification)
+        boot_notification = authorize_request.generate(id_token=authorize_request.get_id_tocken('111', type='Central'))
+        # a = {"idToken": "111", "type": "Central"}
+        # boot_notification = authorize_request.generate(a)
+        print("Sending BootNotification:", boot_notification)
 
         try:
             response = await self.call(boot_notification)
-            print('->Response received>:', repr(response))
+            print('->Response received 芜湖!>:', repr(response))
         except asyncio.TimeoutError:
-            print('Request timed out.')
+            print("Request timed out.")
         except Exception as e:
-            print(f'An error occurred: {e}')
+            print(f"An error occurred: {e}")
 
 
-# WebSocket 客户端连接并发送 BootNotification 消息
 async def main():
-    uri = 'ws://localhost:12345'  # 服务器地址
+    uri = 'ws://localhost:12345'
     client = WebSocketClient(uri)
     charge_point = MyChargePoint('CP1', client)
     async with client:
-        # async def listening():
-        #     while client:
-        #         await client.recv()
-
-        # await charge_point.send_boot_notification()  # 发送消息
-        # await asyncio.create_task(listening())
-        # await asyncio.create_task(await charge_point.send_boot_notification())
-
         async def listen_for_messages():
-            while True:  # 持续接收消息
+            while True:
                 message = await client.recv()
-                print('Received message from server:', message)
+                print("Received message from server:", message)
+                if message is not None and isinstance(message, str) and message.startswith('['):
+                    await charge_point.route_message(message)
 
         listening_task = asyncio.create_task(listen_for_messages())
         await charge_point.send_boot_notification()
-        await asyncio.gather(listening_task)  # 等待监听任务
+        await asyncio.gather(listening_task)
 
-        # await charge_point.send_boot_notification()
         await asyncio.Future()
 
 asyncio.run(main())
--- python_script/服务器测试.py
+++ python_script/服务器测试.py
@@ -7,7 +7,6 @@ from sys_basis.Generator_Ocpp_Std.V2_0_1 import *
 
 
 class MyChargePoint(ChargePoint):
-    # 监听 BootNotification 消息
     @on('Authorize')
     async def on_authorize_request(self, id_token: dict):
         print(f'Received AuthorizeRequest from {id_token}\n')
@@ -16,21 +15,13 @@ class MyChargePoint(ChargePoint):
         )
         return response_obj
 
-# 处理WebSocket连接并启动ChargePoint
-
-
-async def on_connect(websocket):
-    charge_point = MyChargePoint('CP1', websocket)
-    await charge_point.start()  # 启动ChargePoint, 等待消息
-
-# 启动WebSocket服务器
-
 
 async def start_server():
     server = WebSocketServer('localhost', 12345)
     server.signal_recv.connect(out)
+    charge_point = MyChargePoint('CP1', server)
     async with server:
-        await on_connect(server)
+        await charge_point.start()
         await asyncio.Future()
 
 
@@ -38,9 +29,8 @@ def out(message):  # 槽函数
     print(f'out\t{message}')
 
 
-# 创建并运行事件循环
-loop = asyncio.get_event_loop()  # 获取事件循环
-server = loop.run_until_complete(start_server())  # 启动WebSocket服务器
+loop = asyncio.get_event_loop()
+server = loop.run_until_complete(start_server())
 
 # 运行事件循环
 loop.run_forever()
