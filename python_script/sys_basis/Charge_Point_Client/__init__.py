68bfbd4df15e813ee44c9417a6459a071555f01f
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-12 04:55:06 +0100

    新增客户端充电桩的文件

---
+++ python_script/sys_basis/Charge_Point_Client/__init__.py
@@ -0,0 +1,10 @@
+
+""" 
+此模块必须使用显式导入, 禁止使用from . import *
+
+例如: from 上级路径.Charge_Point_Server import ChargePointServerV201
+"""
+from ._Charge_Point_Client_V2_0_1 import ChargePointServerV201
+from ._Charge_Point_Client_V1_6 import ChargePointServerV16
+
+__all__ = []
