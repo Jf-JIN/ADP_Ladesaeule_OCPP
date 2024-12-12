ef168458bd0a42c77e804a224ae49e02eb097759
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-12 04:53:46 +0100

    更新测试文件
    主要对服务器的充电桩基类的功能测试进行更新

--- python_script/客户端测试.py
+++ python_script/客户端测试.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from sys_basis.WebSocket_Client import WebSocketClient
 from ocpp.v201 import ChargePoint
 from sys_basis.Generator_Ocpp_Std.V2_0_1 import *
@@ -6,18 +7,21 @@ from sys_basis.Generator_Ocpp_Std.V2_0_1 import *
 
 class MyChargePoint(ChargePoint):
     async def send_boot_notification(self):
-        boot_notification = authorize_request.generate(id_token=authorize_request.get_id_tocken('111', type='Central'))
+        boot_notification = authorize_request.generate(
+            id_token=authorize_request.get_id_tocken('111', type='Central'),
+            custom_data=authorize_request.get_custom_data(str(time.time()))
+        )
         # a = {"idToken": "111", "type": "Central"}
         # boot_notification = authorize_request.generate(a)
-        print("Sending BootNotification:", boot_notification)
+        print("\nSending BootNotification:", boot_notification)
 
         try:
             response = await self.call(boot_notification)
-            print('->Response received 芜湖!>:', repr(response))
+            print('\n->Response received 芜湖!>:', repr(response))
         except asyncio.TimeoutError:
-            print("Request timed out.")
+            print("\nRequest timed out.")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"\nAn error occurred: {e}")
 
 
 async def main():
@@ -33,6 +37,7 @@ async def main():
                     await charge_point.route_message(message)
 
         listening_task = asyncio.create_task(listen_for_messages())
+        await charge_point.send_boot_notification()
         await charge_point.send_boot_notification()
         await asyncio.gather(listening_task)
 
--- python_script/服务器测试.py
+++ python_script/服务器测试.py
@@ -1,27 +1,36 @@
 import asyncio
-from urllib import response
 from sys_basis.WebSocket_Server import WebSocketServer
 from ocpp.routing import on
-from ocpp.v201 import ChargePoint, call
+from ocpp.v201 import ChargePoint, call, enums
 from sys_basis.Generator_Ocpp_Std.V2_0_1 import *
+from sys_basis.XSignal import XSignal
+import inspect
+from sys_basis.Charge_Point_Server import ChargePointServerV201
+import time
 
 
-class MyChargePoint(ChargePoint):
-    @on('Authorize')
-    async def on_authorize_request(self, id_token: dict):
-        print(f'Received AuthorizeRequest from {id_token}\n')
-        response_obj = authorize_response.generate(
-            authorize_response.get_id_token_info('Accepted'),
-        )
-        return response_obj
+class MyChargePoint_S(ChargePointServerV201):
+    pass
+
+    # @on(Action.authorize)
+    # async def on_authorize_request(self, id_token: dict, custom_data: dict | None = None, certificate: str | None = None, hash_data: list | None = None):
+    #     self._send_signal_info_and_ocpp_message(Action.authorize)
+    #     await asyncio.sleep(5)
+    #     response_obj = authorize_response.generate(
+    #         id_token_info=authorize_response.get_id_token_info('Accepted'),
+    #         custom_data=authorize_response.get_custom_data(str(time.time()))
+    #     )
+    #     return response_obj
 
 
 async def start_server():
     server = WebSocketServer('localhost', 12345)
     server.signal_recv.connect(out)
-    charge_point = MyChargePoint('CP1', server)
+    charge_point = MyChargePoint_S('CP1', server, 5)
+    charge_point.show_current_message_to_send()
     async with server:
         await charge_point.start()
+
         await asyncio.Future()
 
 
@@ -31,6 +40,4 @@ def out(message):  # 槽函数
 
 loop = asyncio.get_event_loop()
 server = loop.run_until_complete(start_server())
-
-# 运行事件循环
 loop.run_forever()
