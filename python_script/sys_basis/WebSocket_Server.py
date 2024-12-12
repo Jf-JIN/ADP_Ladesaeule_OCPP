19dcf801e35e01a7584bfdccf5ba7bf2f10e97ca
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-12 19:15:53 +0100

    Update WebSocket_Server.py
    更新WebSocket客户端
    新增类内信号 signal_recv
    新增发送接收信号函数
    将print统一改用发送信号函数

--- python_script/sys_basis/WebSocket_Server.py
+++ python_script/sys_basis/WebSocket_Server.py
@@ -2,7 +2,6 @@ import asyncio
 import websockets
 from .XSignal import XSignal
 from websockets.asyncio.server import ServerConnection  # 用于类型注释
-from ocpp.messages import unpack, MessageType
 
 
 class WebSocketServer:
@@ -13,46 +12,45 @@ class WebSocketServer:
         self.port = port
         self.__server = None
         self.__clients = set()
-        self.__message_queue = asyncio.Queue()  # 创建消息队列
+        self.__message_queue = asyncio.Queue()
 
     async def __aenter__(self):
         self.__server = await websockets.serve(self.__handle_client, self.host, self.port)
-        print('--<WebSocket_started>\n')
+        self.__send_signal_recv('\n--<WebSocket_started> - <Server>')
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         """
         在上下文管理器退出时关闭服务器
         """
-        # self.__ping_timer.cancel()
         await self.__server.close()
         await self.__server.wait_closed()
-        print('--<WebSocket_closed>\n')
+        self.__send_signal_recv('--<WebSocket_closed>')
 
     async def __handle_client(self, websocket: ServerConnection):
         if websocket not in self.__clients:
             self.__clients.add(websocket)
         await websocket.send(f'<Responde> {websocket.remote_address}')
         await self.send(f'<Responde> {websocket.remote_address}')
-        print(f'--<Client_connected> {websocket.remote_address}\n')
+        self.__send_signal_recv(f'--<Client_connected> {websocket.remote_address}')
         try:
             while True:
                 message = await websocket.recv()
-                print(f'->recieved> {message}\n')
+                self.__send_signal_recv(f'->>> received> {message}')
                 self.signal_recv.emit(message)
                 await self.__filter_for_ocpp(message=message)
         except websockets.ConnectionClosed as e:
-            print(f'--<Connection_closed> {e}\n')
+            self.__send_signal_recv(f'--<Connection_closed> {e}')
 
     async def send(self, message):
         for client in self.__clients.copy():
             client: ServerConnection
             try:
                 await client.ping()
-                print(f'<-send_to<{client.remote_address}\t{message}\n')
+                self.__send_signal_recv(f'<<<- send_to< {client.remote_address}\t{message}')
                 asyncio.create_task(client.send(message))
             except websockets.ConnectionClosed:
-                print(f'--<Client_disconnected> {client.remote_address}\n')
+                self.__send_signal_recv(f'--<Client_disconnected> {client.remote_address}')
                 self.__clients.remove(client)
 
     async def recv(self):
@@ -60,7 +58,6 @@ class WebSocketServer:
         return message
 
     async def __filter_for_ocpp(self, message: str):
-        # print('__filter_for_ocpp\t', repr(message))
         if message in [
             'Successfully connected to the server',
         ]:
@@ -68,6 +65,24 @@ class WebSocketServer:
         if not (isinstance(message, str) and message.startswith('[')):  # 判断是否是Ocpp消息, 其特点为列表
             return
         await self.__message_queue.put(message)
+
+    def __send_signal_recv(self, *args):
+        """
+        发送/打印 接收信号
+
+        涵盖发送前的检查
+
+        参数: 
+        - args: 可变数量的参数, 每个参数都应该是能够被转换为字符串的对象. 建议传递字符串、数字或任何有明确 `__str__` 或 `__repr__` 方法的对象, 以确保能够正确地将参数转换为字符串形式. 
+        """
+        try:
+            temp = ''.join([str(*args)]) + '\n'
+            self.signal_recv.emit(temp)
+            print(temp)
+        except Exception as e:
+            error_text = f'********************\n<Error - send_signal_info> {e}\n********************'
+            self.signal_recv.emit(error_text)
+            print(error_text)
 
 
 if __name__ == '__main__':
