38cd8fa6542b6a468446a8df3078401779d8ed81
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-16 14:43:04 +0100

    增加网页后端线程

---
+++ python_script/sys_basis/XThread/Thread_WebSocket_Server_Port.py
@@ -0,0 +1,159 @@
+
+
+import asyncio
+import traceback
+import json
+from threading import Thread
+from script.sys_basis.XSignal import XSignal
+from sys_basis.WebSocket_Server import WebSocketServer
+
+
+class WebSocketServerPort(Thread):
+    def __init__(self, host, port, info_title='Info_Server_Port'):
+        super().__init__()
+        self.__signal_thread_websocket_client_info = XSignal()
+        self.__signal_thread_websocket_client_recv = XSignal()
+        self.__signal_thread_websocket_client_finished = XSignal()
+        self.__websocket = WebSocketServer(host=host, port=port, info_title='Info_WebSocket_Server')
+        self.__websocket.signal_websocket_server_recv.connect(self.__handle_recv_message)
+        self.__websocket.signal_websocket_server_info.connect(self.signal_thread_websocket_client_info.emit)
+        self.__list_send_data = []  # 存储待发送消息, 当列表非空则持续发送, 当列表为空则相应事件(__event_request_message)等待
+        self.__event_send_data = asyncio.Event()  # 发送消息事件
+        self.__isRunning = True
+        try:
+            if info_title is not None:
+                self.__info_title = str(info_title)
+        except:
+            self.__send_signal_info(f'<Error - __init__> info_title must be convertible to a string. It has been set to None. The provided type is {type(info_title)}')
+            self.__info_title = None
+
+    @property
+    def websocket(self) -> WebSocketServer:
+        return self.__websocket
+
+    @property
+    def isRunning(self) -> bool:
+        return self.__isRunning
+
+    @property
+    def signal_thread_websocket_client_info(self) -> XSignal:
+        return self.__signal_thread_websocket_client_info
+
+    @property
+    def signal_thread_websocket_client_recv(self) -> XSignal:
+        return self.__signal_thread_websocket_client_recv
+
+    @property
+    def signal_thread_websocket_client_finished(self) -> XSignal:
+        return self.__signal_thread_websocket_client_finished
+
+    def send_data(self, data):
+        self.__list_send_data.append(data)
+        self.__event_send_data.set()
+
+    def __send_signal_info(self, *args) -> None:
+        """
+        发送/打印 信息信号
+
+        涵盖发送前的检查
+
+        参数:
+        - args: 可变数量的参数, 每个参数都应该是能够被转换为字符串的对象. 建议传递字符串、数字或任何有明确 `__str__` 或 `__repr__` 方法的对象, 以确保能够正确地将参数转换为字符串形式.
+        """
+        self.__send_signal(signal=self.__signal_thread_websocket_client_info, error_hint='send_signal_info', log=None, doShowTitle=True, doPrintInfo=True, args=args)
+
+    def __send_signal(self, signal: XSignal, error_hint: str, log=None, doShowTitle: bool = False, doPrintInfo: bool = False, args=[]) -> None:
+        """
+        发送/打印 信号
+
+        涵盖发送前的检查
+
+        参数:
+        - signal(XSignal): 信号对象
+        - error_hint(str): 错误提示
+        - log: 日志器动作
+        - doShowTitle(bool): 是否显示标题
+        - doPrintInfo(bool): 是否打印信息
+        - args: 元组或列表或可解包对象, 每个参数都应该是能够被转换为字符串的对象. 建议传递字符串、数字或任何有明确 `__str__` 或 `__repr__` 方法的对象, 以确保能够正确地将参数转换为字符串形式.
+        """
+        try:
+            temp = ''.join([str(*args)]) + '\n'
+            if self.__info_title and doShowTitle:
+                temp = f'< {self.__info_title} >\n' + temp
+            signal.emit(temp)
+            if doPrintInfo:
+                print(temp)
+            if log:
+                log(temp)
+        except Exception as e:
+            error_text = f'********************\n<Error - {error_hint}> {e}\n********************'
+            if self.__info_title and doShowTitle:
+                error_text = f'< {self.__info_title} >\n' + error_text
+            signal.emit(error_text)
+            if doPrintInfo:
+                print(error_text)
+            if log:
+                log(temp)
+
+    def __handle_recv_message(self, message: str) -> None:
+        """
+        处理接收到的消息
+
+        消息内容将通过信号 signal_thread_websocket_client_recv 发送出去, 其类型可能为 str 或 dict
+
+        参数: 
+        - message: 接收到的消息
+        """
+        if message.startswith(f='{'):
+            try:
+                message = json.loads(message)
+            except Exception as e:
+                self.__send_signal_info(f'<Error> {e}')
+        self.__send_signal_info(f'->>> Received> {message}')
+        self.signal_thread_websocket_client_recv.emit(message)
+
+    async def __send_data(self) -> None:
+        """
+        发送消息, 循环执行
+
+        当 send_data 被调用时, 会将消息放入队列中, 然后通过此方法发送
+
+        当信息列表 __list_send_data 为空时, 将等待事件 __event_send_data 触发
+        """
+        while self.__isRunning:
+            await self.__event_send_data.wait()
+            if not self.__isRunning:  # 提前终止
+                break
+            if self.__list_send_data:
+                message = self.__list_send_data.pop(0)
+                await self.__websocket.send(message)
+                self.__send_signal_info(f'<<<- Sent> {message}')
+            if len(self.__list_send_data) == 0:
+                self.__event_send_data.clear()
+
+    def stop(self) -> None:
+        """
+        终止线程
+
+        `__isRunning` 将设置为 `False` , 并取消所有任务
+        所有阻塞的信号量将被释放
+        """
+        self.__isRunning = False
+        self.__event_send_data.set()
+        for task in [self.__task_send_data]:
+            if task:
+                task.cancel()
+                self.__send_signal_info(f'<Task Cancel> {task.task_name} is canceled')
+        self.__signal_thread_websocket_client_finished.emit()
+
+    async def __start_loop(self) -> None:
+        """
+        异步协程主体
+        """
+        self.__task_send_data = asyncio.create_task(self.__send_data())
+        setattr(self.__task_send_data, 'task_name', 'task_send_data')
+        await asyncio.gather(self.__task_send_data)
+        await asyncio.Future()
+
+    def run(self):
+        asyncio.run(self.__start_loop())
