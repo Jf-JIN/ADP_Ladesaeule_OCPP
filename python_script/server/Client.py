a45cfdf006f75dd4d3ac7de519d9a853978a4938
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-12-20 00:17:32 +0100

    Create Client.py

---
+++ python_script/server/Client.py
@@ -0,0 +1,67 @@
+
+from sys_basis.Ports import *
+from sys_basis.XSignal import XSignal
+from sys_basis.Manager_Coroutine import ManagerCoroutines
+
+
+class Client:
+    def __init__(self):
+        self.__init_signals()
+        self.__init_parameters()
+        self.__init_threads()
+        self.__init_coroutines()
+        self.__init_signal_connections()
+        self.__thread_web_server.start()
+        self.__manager_coroutines.start()
+
+        # self.__coroutine_OCPP_client.start()
+
+        # self.__coroutine_gui_websocket_server.start()
+
+    def __init_signals(self):
+        pass
+
+    def __init_signal_connections(self):
+        self.__coroutine_OCPP_client.signal_thread_ocpp_client_info.connect(self.__send_info_client_message)
+        self.__coroutine_OCPP_client.signal_thread_ocpp_client_recv.connect(self.__send_info_opt_message)
+        # self.__coroutine_OCPP_client.signal_thread_ocpp_client_recv_request
+        # self.__coroutine_OCPP_client.signal_thread_ocpp_client_recv_response
+        self.__thread_web_server.signal_thread_web_server_info.connect(self.__send_info_client_message)
+        self.__thread_web_server.signal_thread_web_server_recv.connect(self.__send_info_client_message)
+        # self.__thread_web_server.signal_thread_webs_server_finished
+        self.__coroutine_gui_websocket_server.signal_thread_websocket_client_info.connect(self.__send_info_gui_message)
+        # self.__coroutine_gui_websocket_server.signal_thread_websocket_client_recv
+        pass
+
+    def __init_parameters(self):
+        pass
+
+    def __init_threads(self):
+
+        self.__thread_web_server = PortWebServer()
+
+    def __init_coroutines(self):
+        self.__coroutine_OCPP_client = PortOCPPWebsocketClient('ws://localhost:12345', 'CP1')
+        self.__coroutine_gui_websocket_server = PortWebSocketServer('localhost', 12346)
+        self.__manager_coroutines = ManagerCoroutines(self.__coroutine_OCPP_client.run, self.__coroutine_gui_websocket_server.run)
+
+    def __send_info_client_message(self, message):
+        temp_dict = {
+            'client_console': message
+        }
+        self.__thread_web_server.send_message(temp_dict)
+
+    def __send_info_opt_message(self, message):
+        temp_dict = {
+            'opt_console': message
+        }
+        self.__thread_web_server.send_message(temp_dict)
+
+    def __send_info_gui_message(self, message):
+        temp_dict = {
+            'gui_websocket_console': message
+        }
+        self.__thread_web_server.send_message(temp_dict)
+
+    def handle_web_server_message(self, message):
+        pass
