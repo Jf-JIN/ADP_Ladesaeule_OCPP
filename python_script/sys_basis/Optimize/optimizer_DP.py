11b08a3a8c1827d414711caa02d035a3d8b9d72d
zchasing <zchasing97@gmail.com>
2024-12-22 13:13:14 +0100

    更新一部分优化器

--- python_script/sys_basis/Optimize/optimizer_DP.py
+++ python_script/sys_basis/Optimize/optimizer_DP.py
@@ -1,3 +1,6 @@
+from ChargingNeedsRequest import *
+from ChargingScheduleRequest import *
+import time
 
 
 class OptimizerDP:
@@ -11,14 +14,19 @@ class OptimizerDP:
         - custom_data(dict): 自定义数据, 推荐使用 `get_custom_data()` 传入
         - max_schedule_tuples(int): 车辆支持的每张计划表可容纳的最大元祖数 
     """
+
     def __init__(self, charging_needs_request):
-        self.evse_id = charging_needs_request['evseId']
-        self.charging_needs = charging_needs_request['chargingNeeds']
-        self.custom_data = charging_needs_request.get("customData", None)
-        self.max_schedule_tuples = charging_needs_request.get("maxScheduleTuples", None)
+        self.cnr = ChargingNeedsRequest
+        self.cnr.evse_id = charging_needs_request['evseId']
+        self.cnr.charging_needs = charging_needs_request['chargingNeeds']
+        self.cnr.custom_data = charging_needs_request.get("customData", None)
+        self.cnr.max_schedule_tuples = charging_needs_request.get("maxScheduleTuples", None)
 
         # 初始化充电计划
         self._charging_schedule = []
+
+    def get_charging_needs(self):
+        return self.cnr.charging_needs
 
     def get_charging_schedule(self):
         """
@@ -35,4 +43,27 @@ class OptimizerDP:
         """
         使用动态规划算法生成充电计划。
         """
-        charging_needs = self.charging_needs
+        charging_needs = self.cnr.charging_needs
+
+
+if __name__ == "__main__":
+    charging_needs_request = {
+        "evseId": 1,
+        "chargingNeeds": {
+            "requestedEnergyTransfer": EnergyTransferModeType.ac_three_phase,
+            "acChargingParameters": {
+                "energyAmount": 100000,
+                "evMaxCurrent": 32,
+                "evMinCurrent": 6,
+                "evMaxVoltage": 400,
+                "customData": {
+                    "vendorId": "12",
+                    "stateOfCharge": 0
+                }
+            },
+            "departureTime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 8 * 3600))
+        }
+    }
+    print(charging_needs_request)
+    op_dp = OptimizerDP(charging_needs_request)
+    print(op_dp.get_charging_needs())
