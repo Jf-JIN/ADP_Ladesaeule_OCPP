3c3691305c127c419185cc6662f9c66c72c3c4e8
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-01-03 10:31:51 +0100

    新增对额外关键字的扩展
    Co-Authored-By: zchasing <82604688+zchasing@users.noreply.github.com>

--- python_script/sys_basis/Generator_Ocpp_Std/V2_0_1/_Base.py
+++ python_script/sys_basis/Generator_Ocpp_Std/V2_0_1/_Base.py
@@ -1,16 +1,19 @@
 
 class Base_OCPP_Struct_V2_0_1(object):
     @staticmethod
-    def get_custom_data(vendor_id: str) -> dict:
+    def get_custom_data(vendor_id: str, **kwargs) -> dict:
         """
         生成 CustomData
 
         参数:
             vendor_id(str): 厂商ID (1-255 个字符)
+            **kwargs: 额外的关键字参数内容
 
         返回值: 
             CustomData(dict)
         """
-        return {
+        custom_data = {
             "vendorId": vendor_id
         }
+        custom_data.update(kwargs)  # 将额外的关键字参数加入字典
+        return custom_data
