8172792860e32876cd5dcc9571dcf30bc56e0990
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-01-04 07:51:58 +0100

    Update Opt_Parameter.py

--- python_script/const/Opt_Parameter.py
+++ python_script/const/Opt_Parameter.py
@@ -9,3 +9,5 @@ APP_WORKSPACE_PATH = os.getcwd()
 
 class OptParams(AnalogDefine):
     CHARGING_NEEDS_REQUEST_INTERVAL = 3600  # seconds
+    OCPP_WEBSOCKET_TIMEOUT = 30  # seconds
+    OCPP_WEBSOCKET_PORT = 12345
