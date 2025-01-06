eb4ab5e06f8c6890ea3d46465c68147cd024810d
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-01-06 05:04:40 +0100

    删除多余的代码

--- python_script/const/Opt_Parameter.py
+++ python_script/const/Opt_Parameter.py
@@ -3,10 +3,6 @@ from const.Analog_Define import AnalogDefine
 import os
 
 
-os.chdir(os.path.dirname(os.path.dirname(__file__)))
-APP_WORKSPACE_PATH = os.getcwd()
-
-
 class OptParams(AnalogDefine):
     CHARGING_NEEDS_REQUEST_INTERVAL = 3600  # seconds
     OCPP_WEBSOCKET_TIMEOUT = 30  # seconds
