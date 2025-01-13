6d84ef875b0aa6c0bcb5d39cf77960343df4b726
Codesoap95 <jiangyunan95@foxmail.com>
2025-01-13 20:14:22 +0100

    GPIO部分
    增加了GPIOmanager，Shelly manager，shelly线程

--- python_script/sys_basis/GPIO/_GPIO_Manager.py
+++ python_script/sys_basis/GPIO/_GPIO_Manager.py
@@ -1,33 +1,101 @@
+from sys_basis.XSignal import XSignal
+from _EVSE_manager import EVSEManager
+from _Shelly_manager import ShellyManager
 
-import time
 
-from _EVSE_communication import EVSECommunication
+class GPIOManager:
+    """
+    参数:
+    - RCD: bool, 是否安装RCD
+    - new_charge: bool, 是否是新的一次充电
+    信号:
+    -   __signal_GPIO_min_current: 允许最小电流
+    -   __signal_GPIO_actual_current: EVSE实际电流
+    -   __signal_GPIO_EVSE_failure: EVSE故障
+    -   __signal_GPIO_vehicle_state: 车辆状态
+    -   __signal_GPIO_Shelly_error: Shelly故障
+    -   __signal_GPIO_Shelly_power: Shelly功率
+    -   __signal_GPIO_Shelly_power_factor: Shelly功率因数
+    -   __signal_GPIO_Shelly_current: Shelly电流
+    -   __signal_GPIO_Shelly_voltage: Shelly电压
+    -   __signal_GPIO_Shelly_total: Shelly已充电Wh数
+    -   __signal_GPIO_Shelly_total_returned: Shelly返回充电桩的总能量
+    方法:
+    - set_current: 设置EVSE输出电流
+    """
+    def __init__(self,RCD: bool,new_charge: bool):
+        self.evse = EVSEManager(RCD)
+        self.shelly = ShellyManager(new_charge)
 
+        self.__signal_GPIO_min_current = XSignal()
+        self.__signal_GPIO_actual_current = XSignal()
+        self.__signal_GPIO_EVSE_failure = XSignal()
+        self.__signal_GPIO_vehicle_state = XSignal()
+        self.__signal_GPIO_Shelly_error = XSignal()
+        self.__signal_GPIO_Shelly_power = XSignal()
+        self.__signal_GPIO_Shelly_power_factor = XSignal()
+        self.__signal_GPIO_Shelly_current = XSignal()
+        self.__signal_GPIO_Shelly_voltage = XSignal()
+        self.__signal_GPIO_Shelly_total = XSignal()
+        self.__signal_GPIO_Shelly_total_returned = XSignal()
 
-class GPIO_Manager:
+        self.evse.signal_EVSE_min_current.connect(self.signal_GPIO_min_current.emit)
+        self.evse.signal_EVSE_actual_current.connect(self.signal_GPIO_actual_current.emit)
+        self.evse.signal_EVSE_failure.connect(self.signal_GPIO_EVSE_failure.emit)
+        self.evse.signal_vehicle_state.connect(self.signal_GPIO_vehicle_state.emit)
+        self.shelly.signal_Shelly_error.connect(self.signal_GPIO_Shelly_error.emit)
+        self.shelly.signal_Shelly_power.connect(self.signal_GPIO_Shelly_power.emit)
+        self.shelly.signal_Shelly_pf.connect(self.signal_GPIO_Shelly_power_factor.emit)
+        self.shelly.signal_Shelly_current.connect(self.signal_GPIO_Shelly_current.emit)
+        self.shelly.signal_Shelly_voltage.connect(self.signal_GPIO_Shelly_voltage.emit)
+        self.shelly.signal_Shelly_total.connect(self.signal_GPIO_Shelly_total.emit)
+        self.shelly.signal_Shelly_total_returned.connect(self.signal_GPIO_Shelly_total_returned.emit)
 
-    def __init__(self):
-        self.evse = EVSECommunication()
-        self.__EVSE_Failure = False
+    @property
+    def signal_GPIO_min_current(self):
+        return self.__signal_GPIO_min_current
+
+    @property
+    def signal_GPIO_actual_current(self):
+        return self.__signal_GPIO_actual_current
+
+    @property
+    def signal_GPIO_EVSE_failure(self):
+        return self.__signal_GPIO_EVSE_failure
+
+    @property
+    def signal_GPIO_vehicle_state(self):
+        return self.__signal_GPIO_vehicle_state
+
+    @property
+    def signal_GPIO_Shelly_error(self):
+        return self.__signal_GPIO_Shelly_error
+
+    @property
+    def signal_GPIO_Shelly_power(self):
+        return self.__signal_GPIO_Shelly_power
+
+    @property
+    def signal_GPIO_Shelly_power_factor(self):
+        return self.__signal_GPIO_Shelly_power_factor
+
+    @property
+    def signal_GPIO_Shelly_current(self):
+        return self.__signal_GPIO_Shelly_current
+
+    @property
+    def signal_GPIO_Shelly_voltage(self):
+        return self.__signal_GPIO_Shelly_voltage
+
+    @property
+    def signal_GPIO_Shelly_total(self):
+        return self.__signal_GPIO_Shelly_total
+
+    @property
+    def signal_GPIO_Shelly_total_returned(self):
+        return self.__signal_GPIO_Shelly_total_returned
+
 
     def set_current(self,current):
         self.evse.set_current(current)
-# 主程序
-if __name__ == "__main__":
-    def print_time(current_time):
-        print(f"收到时间信号: {current_time}")
 
-
-    t_thread = TimeThread(interval=1)
-    t_thread.signal_currentTime.connect(print_time)  # 连接槽
-    t_thread.start()
-
-    try:
-        # 主线程继续运行，等待用户手动终止
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        print("正在停止线程...")
-        t_thread.stop()
-        t_thread.join()
-        print("线程已停止。")
--- python_script/sys_basis/GPIO/_Thread_Shelly_Reader.py
+++ python_script/sys_basis/GPIO/_Thread_Shelly_Reader.py
@@ -1,28 +1,55 @@
 import traceback
-
+from const.GPIO_Parameter import GPIOParams
 import requests
+from sys_basis.XSignal  import XSignal
+from threading import Thread
+from const.Const_Parameter import *
+import time
 
+_info = Log.GPIO
 
-class GetShellyData:
+class GetShellyData(Thread):
 
+    def __init__(self):
+        super().__init__()
+        self.__interval = GPIOParams.POLL_INTERVAL_SHELLY  # 每隔多少秒返回时间
+        self.__running = True  # 控制线程是否继续运行
+        self.__Signal_Shelly_data = XSignal()
+        self.__Signal_Shelly_error = XSignal()
+    # 替换为实际的 Shelly 3EM 设备 IP 地址
+        self.__shelly_ip = GPIOParams.SHELLY_IP
+        self.__emeter_index = GPIOParams.SHELLY_EMETER_INDEX
+        self.__emeter_url = f"http://{self.__shelly_ip}/emeter/{self.__emeter_index}"
 
-# 替换为实际的 Shelly 3EM 设备 IP 地址
-shelly_ip = "192.168.1.100"
-emeter_index = 0  # 通道索引：0、1 或 2
-emeter_url = f"http://{shelly_ip}/emeter/{emeter_index}"
+    @property
+    def signal_Shelly_data(self) -> XSignal:
+        return self.__Signal_Shelly_data
 
-try:
-#请求失败后的处理
-    response = requests.get(emeter_url, timeout=5)
-    response.raise_for_status()
-    data = response.json()
-    print(f"通道 {emeter_index} 的电能数据：", data)
-except requests.exceptions.RequestException as e:
-    print(f"请求失败: {traceback.format_exc()}")
-#except Exception 断连，重连等等
+    @property
+    def signal_Shelly_error(self) -> XSignal:
+        return self.__Signal_Shelly_error
 
-# 假设已经获取了 emeter 数据
-voltage = data.get("voltage")
-current = data.get("current")
-power = data.get("power")
-print(f"电压: {voltage} V, 电流: {current} A, 功率: {power} W")
+    def get_data(self):
+        try:
+            #请求失败后的处理
+            response = requests.get(self.__emeter_url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
+            _info(f"通道 {self.__emeter_index} 的电能数据：", data)
+            return data
+        except requests.exceptions.RequestException as e:
+            _info(f"请求失败: {traceback.format_exc()}")
+            self.signal_Shelly_error.emit(True)
+            return None
+
+    def run(self):
+        while self.__running:
+            data = self.get_data()
+            if data:
+                self.signal_Shelly_data.emit(data)
+                _info(f"Shelly数据：{data}")
+            time.sleep(self.__interval)
+
+    def stop(self):
+        self.__isRunning = False
+        self.join()  # 等待线程结束
