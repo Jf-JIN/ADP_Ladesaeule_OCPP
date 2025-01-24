fed3c4728d2e8dc6343455562b2a2d037ec3d073
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-01-24 13:55:42 +0100

    更新代码
    补全了ModbusIO
    重命名了两个轮询线程
    补全了两个轮询线程
    更改了GPIO项目图

---
+++ python_script/sys_basis/GPIO/_Thread_Polling_EVSE.py
@@ -0,0 +1,61 @@
+
+from threading import Thread
+
+from sys_basis.GPIO._GPIO_Manager import GPIOManager
+from ._Modbus_IO import ModbusIO
+from const.GPIO_Parameter import *
+from const.Const_Parameter import *
+import time
+
+
+if 0:
+    from ._GPIO_Manager import GPIOManager
+    from ._Data_Collector import DataCollector
+    from ._EVSE import Evse
+
+_debug = Log.EVSE.debug
+_info = Log.EVSE.info
+_exception = Log.EVSE.exception
+
+
+class PollingEVSE(Thread):
+    def __init__(self, parent: GPIOManager, *, charge_unit_dict: dict, intervall: int | float) -> None:
+        super().__init__()
+        self.__parent: GPIOManager = parent
+        self.__evse_list: list = []
+        for item in charge_unit_dict.values():
+            self.__evse_list.append(item['evse'])
+        self.__evse_quantity: int = len(self.__evse_list)
+        self.__interval: int | float = intervall
+        self.__data_collector: DataCollector = self.__parent.data_collector
+        self.__isRunning: bool = True
+        self.__current_index: int = 0
+
+    @property
+    def isRunning(self) -> bool:
+        return self.__isRunning
+
+    def run(self) -> None:
+        while self.__isRunning:
+            evse: Evse = self.__evse_list[self.__current_index]
+            evse_id: int = evse.id
+            evse_data = {}
+            with ModbusIO(id=evse_id) as io:
+                vehicle_state: int | None = io.read_vehicle_status()  # timeout 由 Modbus 决定
+                if vehicle_state is not None:
+                    evse_data['vehicle_state'] = vehicle_state
+
+                evse_error = io.read_evse_status_and_fails()
+                if evse_error is not None:
+                    evse_data['evse_error'] = evse_error
+                    evse.set_evse_status_error(evse_error)
+
+                if len(evse_data):
+                    self.__data_collector.set_evse_data(evse_id, evse_data)
+                    evse.set_data(evse_data)
+            self.__current_index = (self.__current_index + 1) % self.__evse_quantity
+            time.sleep(self.__interval)
+
+    def stop(self) -> None:
+        self.__isRunning = False
+        self.join()
