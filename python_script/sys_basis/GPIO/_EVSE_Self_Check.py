f469f7e8ddbc14425656c4fbc4348ac000284644
Codesoap95 <jiangyunan95@foxmail.com>
2025-01-24 15:47:56 +0100

    补全完善代码

--- python_script/sys_basis/GPIO/_EVSE_Self_Check.py
+++ python_script/sys_basis/GPIO/_EVSE_Self_Check.py
@@ -9,15 +9,14 @@ import time
 _info = Log.GPIO
 
 class EVSESelfCheck(Thread):
-    def __init__(self,id,doUseRCD):
+    def __init__(self,id:int,doUseRCD:bool=False)->None:
         super().__init__()
-
-        self.__id = id
-        self.__timeout = GPIOParams.SELF_CHECK_TIMEOUT
-        self.__signal_self_test_error = XSignal()
-        self.__running = True
-        self.modbus = ModbusIO(id)
-        self.__doUseRCD = doUseRCD
+        self.__id:int = id
+        self.__timeout:int|float = GPIOParams.SELF_CHECK_TIMEOUT
+        self.__signal_self_test_error:XSignal= XSignal()
+        self.__running:bool = True
+        self.__modbus:ModbusIO = ModbusIO(id)
+        self.__doUseRCD:bool = doUseRCD
 
     @property
     def id(self):
@@ -34,30 +33,26 @@ class EVSESelfCheck(Thread):
         """
         return self.__signal_self_test_error.emit(error_index)
 
-    def set_selfcheck(self):
-        with self.modbus as modbus:
-            response = modbus.write_reg1004(bit = 1,flag = 1 )
-        return response
 
     def run(self, ):
         while self.__running:
-            self.set_selfcheck()
-            time.sleep(self.__timeout)
-            with self.modbus as modbus:
-                response = modbus.read_evse_status_fails()
-                if self.__doUseRCD:
-                    if response:
-                        if EVSEFails.RCD_CHECK_ERROR in response:
-                            success_clear = modbus.write(address = 1004, value = REG1004.CLEAR_RCD_ERROR)
-                            if not success_clear:
-                                #这里应该反馈通讯错误？
-                                return False
-                        else :
-                            #如果没有正常显示RCD错误，则认为RCD测试失败
-                            self.signal_self_test_error(set('RCD测试失败'))
-                            return False
-                    else:
-                        self.signal_self_test_error(response)
+            with self.__modbus as modbus:
+                selftest_successful_start = modbus.run_selftest_and_RCD_test_procedure()
+                if selftest_successful_start:
+                    time.sleep(self.__timeout)
+                    response = modbus.read_evse_status_fails()
+                    if self.__doUseRCD:
+                        if response:
+                            if 'RCD Check Error' in response:
+                                success_clear = modbus.write(address = EVSERegAddress.TURN_OFF_SELFTEST_OPERATION, value = BitsFlag.REG1004.CLEAR_RCD_ERROR)
+                                if not success_clear:
+                                    #这里应该反馈通讯错误？
+                                    return False
+                            else :
+                                #如果没有正常显示RCD错误，则认为RCD测试失败
+                                self.signal_self_test_error(set('RCD测试失败'))
+                        else:
+                            self.signal_self_test_error(response)
 
 
 
