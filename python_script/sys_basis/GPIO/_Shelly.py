74c8056281b18f7e254ea81a88476863627eefea
Codesoap95 <jiangyunan95@foxmail.com>
2025-01-24 18:22:21 +0100

    Update _Shelly.py

--- python_script/sys_basis/GPIO/_Shelly.py
+++ python_script/sys_basis/GPIO/_Shelly.py
@@ -1,33 +1,25 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+
+import copy
 from const.GPIO_Parameter import GPIOParams
 from const.Const_Parameter import *
 import requests
 from sys_basis.XSignal  import XSignal
-from _Polling_Shelly import PollingShelly
 
 _info = Log.GPIO
 
 class Shelly:
-    def __init__(self,id):
-
-        #
-        self.address_list = None
-        #
-        #self.signal_current_no = None
-        #self.signal_current_overload = None
-        self.__id = None
-        self.__main_address = GPIOParams.SHELLY_IP[self.__id]
-        self.__data = {}
-        self.__isAvailable = None
-        self.__charged_energy = None
-        #self.__signal_current_no = None
-        #self.__signal_current_overload = None
-        self.__signal_shelly_error_occured = None
+    def __init__(self,id:int,address:str):
+        self.__id:int = id
+        self.__main_address:str = address
+        self.__data:dict = {}
+        self.__isAvailable:bool = True
+        self.__charged_energy:int = 0
+        self.__signal_current_no = XSignal()
+        self.__signal_current_overload = XSignal()
+        self.__signal_shelly_error_occured = XSignal()
         self.__sub_address_0 = f"http://{self.__main_address}/emeter/0"
         self.__sub_address_1 = f"http://{self.__main_address}/emeter/1"
         self.__sub_address_2 = f"http://{self.__main_address}/emeter/2"
-        self.shelly = PollingShelly
 
 
     @property
@@ -51,6 +43,10 @@ class Shelly:
         return self.__sub_address_2
 
     @property
+    def address_list(self):
+        return copy.deepcopy([self.__sub_address_0, self.__sub_address_1, self.__sub_address_2])
+
+    @property
     def isAvailable(self):
         return self.__isAvailable
 
@@ -60,41 +56,40 @@ class Shelly:
 
     @property
     def data(self):
-        return self.__data
+        return copy.deepcopy(self.__data)
 
     @property
     def data_ph0(self):
-        return self.__data['0']
+        return copy.deepcopy(self.__data[0])
 
     @property
     def data_ph1(self):
-        return self.__data['1']
+        return copy.deepcopy(self.__data[1])
 
     @property
     def data_ph2(self):
-        return self.__data['2']
+        return copy.deepcopy(self.__data[2])
 
     @property
     def signal_shelly_error_occured(self):
-        return self.__signal_shelly_error_occured.emit(True)
+        return self.__signal_shelly_error_occured
 
-
-
-    def set_data(self, data):
+    def set_data(self, data:dict):
         self.__data = data
-        self.__charged_energy = self.__data['0']['total'] + self.__data['1']['total'] + self.__data['2']['total']
-        self.__isAvailable =
-        pass
+        self.__charged_energy = self.__data['charged_energy']
+        self.__isAvailable = self.__data['is_valid']
+        if not self.__isAvailable:
+            self.signal_shelly_error_occured.emit()
 
-    def reset(self, ):
-
+    def reset(self):
         try:
             # 发送 POST 请求
-            response0 = requests.post(f"http://{self.__main_address}/emeter/0/reset_totals", timeout=5)
+            reset_token = 'reset_totals'
+            response0 = requests.post(f"{self.__sub_address_0}/{reset_token}", timeout=5)
             response0.raise_for_status()
-            response1 = requests.post(f"http://{self.__main_address}/emeter/1/reset_totals", timeout=5)
+            response1 = requests.post(f"{self.__sub_address_1}/{reset_token}", timeout=5)
             response1.raise_for_status()
-            response2 = requests.post(f"http://{self.__main_address}/emeter/2/reset_totals", timeout=5)
+            response2 = requests.post(f"{self.__sub_address_2}/{reset_token}", timeout=5)
             response2.raise_for_status()
             _info("复位成功! ")
         except requests.exceptions.RequestException as e:
