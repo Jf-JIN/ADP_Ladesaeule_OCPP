55e0cce29de502980b17b64200828ce9db81d206
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-01-28 13:29:29 +0100

    测试时的ip修改

--- python_script/const/Charge_Point_Parameters.py
+++ python_script/const/Charge_Point_Parameters.py
@@ -25,7 +25,8 @@ class CP_Params(AnalogDefine):
     RESPONSE_TIMEOUT = 30  # 响应超时时间, 作用于充电桩
     OCPP_LISTEN_INTERVAL = 1  # OCPP监听间隔时间
     DO_SEND_DEFAULT_RESPONSE = False  # 是否在规定的响应时间内发送默认响应
-    HOST = 'ws://130.83.148.29'  # 优化算法器OCPP服务器地址
+    # HOST = 'ws://130.83.148.29'  # 优化算法器OCPP服务器地址
+    HOST = 'ws://172.18.9.26'  # 优化算法器OCPP服务器地址
     PORT = 80
     # url = 'ws://192.168.43.65:1234'
     # url = 'ws://172.12.26.71:1234'
