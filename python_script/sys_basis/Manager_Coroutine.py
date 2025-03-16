70ad5455969674b400aa7f6c5bea1bfc112cf0d3
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-03-16 14:07:00 +0100

    Update Manager_Coroutine.py
    增加对象销毁方法
    增加停止方法

--- python_script/sys_basis/Manager_Coroutine.py
+++ python_script/sys_basis/Manager_Coroutine.py
@@ -1,14 +1,24 @@
-
+from __future__ import annotations
 import asyncio
+
+from sys_basis.Ports import *
 
 
 class ManagerCoroutines(object):
-    def __init__(self, *task_func):
-        self.__tasks = task_func
+    def __init__(self, *task_obj):
+        self.__task_obj_tuple = task_obj
 
     async def __load_tasks(self) -> None:
-        tasks = [task_func() for task_func in self.__tasks]
+        tasks = [task_func.run() for task_func in self.__task_obj_tuple]
         await asyncio.gather(*tasks)
 
     def start(self) -> None:
         asyncio.run(self.__load_tasks())
+
+    def stop(self) -> None:
+        for task_obj in self.__task_obj_tuple:
+            task_obj: PortOCPPWebsocketClient | PortWebSocketServer
+            task_obj.close()
+
+    def __del__(self) -> None:
+        self.stop()
