3d02b4d6add02f38a72a4d91b4e2766581f84e7d
zchasing <zchasing97@gmail.com>
2025-04-09 23:03:13 +0200

    update

---
+++ python_script/test/EVSE_Test/const/ConstModbus.py
@@ -0,0 +1,10 @@
+from const.Analog_Define import AnalogDefine
+
+class ModbusParams(AnalogDefine):
+    PORT = '/dev/ttyS0'  # 串口号
+    BAUDRATE = 9600  # 波特率
+    PARITY = 'N'  # 无校验位
+    STOPBITS = 1  # 停止位
+    BYTESIZE = 8  # 数据位
+    TIMEOUT = 3  # 超时时间
+    RETRIES = 10  # 重试次数
--- python_script/test/EVSE_Test/const/Const_Logger.py
+++ python_script/test/EVSE_Test/const/Const_Logger.py
@@ -3,3 +3,4 @@ from DToolslib import *
 
 class Log(StaticEnum):
     SOCKET = Logger('Socket_Core', log_level=LogLevel.DEBUG)
+    MODBUS = Logger('Modbus_Handler', log_level=LogLevel.DEBUG)
