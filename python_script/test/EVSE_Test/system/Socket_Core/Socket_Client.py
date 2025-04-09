228ea03e1865a2f726f46979d44e673eb321fd9d
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-04-09 21:57:18 +0200

    Update Socket_Client.py

---
+++ python_script/test/EVSE_Test/system/Socket_Core/Socket_Client.py
@@ -0,0 +1,14 @@
+from DToolslib import *
+from Socket_Core import *
+from const.ConstLogger import *
+
+
+class SocketClient:
+    def __init__(self) -> None:
+        ...
+
+    def connect(self):
+        ...
+
+    def disconnect(self):
+        ...
