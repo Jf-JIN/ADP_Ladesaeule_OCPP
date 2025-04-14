c8427e4851d5a1bf604e860410f278eb7a7f9311
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-04-14 11:41:43 +0200

    Update Const_Parameter.py

--- python_script/const/Const_Parameter.py
+++ python_script/const/Const_Parameter.py
@@ -3,6 +3,7 @@ import os
 from const.Analog_Define import AnalogDefine
 # from tools.Logger import LogLevel, Logger, LoggerGroup
 from DToolslib.Logger import *
+from DToolslib import *
 
 
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
@@ -37,28 +38,34 @@ class Style(AnalogDefine):
     PLOT = 1
 
 
-class Log(AnalogDefine):
-    RAS = Logger(log_name='client_rasberryPi', log_folder_path=APP_WORKSPACE_PATH)
-    CSMS = Logger(log_name='csms', log_folder_path=APP_WORKSPACE_PATH)
-    WS = Logger(log_name='websocket', log_folder_path=APP_WORKSPACE_PATH)
-    CP = Logger(log_name='charge_point', log_folder_path=APP_WORKSPACE_PATH)
-    OCPP = Logger(log_name='ocpp_port', log_folder_path=APP_WORKSPACE_PATH)
-    WEB = Logger(log_name='web', log_folder_path=APP_WORKSPACE_PATH)
-    GPIO = Logger(log_name='gpio', log_folder_path=APP_WORKSPACE_PATH)
-    MODBUS = Logger(log_name='modbus', log_folder_path=APP_WORKSPACE_PATH)
-    SHELLY = Logger(log_name='shelly', log_folder_path=APP_WORKSPACE_PATH)
-    EVSE = Logger(log_name='evse', log_folder_path=APP_WORKSPACE_PATH)
-    OPT = Logger(log_name='optimize', log_folder_path=APP_WORKSPACE_PATH)
-    GUI = Logger(log_name='gui_port', log_folder_path=APP_WORKSPACE_PATH)
-    CSVLoader = Logger(log_name='CSV_loader', log_folder_path=APP_WORKSPACE_PATH)
-    GROUP = LoggerGroup(log_folder_path=APP_WORKSPACE_PATH, limit_files_count=30, limit_single_file_size_kB=100)
+class Log(StaticEnum):
+    RAS = Logger(log_name='client_rasberryPi', root_dir=APP_WORKSPACE_PATH)
+    CSMS = Logger(log_name='csms', root_dir=APP_WORKSPACE_PATH)
+    WS = Logger(log_name='websocket', root_dir=APP_WORKSPACE_PATH)
+    CP = Logger(log_name='charge_point', root_dir=APP_WORKSPACE_PATH)
+    OCPP = Logger(log_name='ocpp_port', root_dir=APP_WORKSPACE_PATH)
+    WEB = Logger(log_name='web', root_dir=APP_WORKSPACE_PATH)
+    GPIO = Logger(log_name='gpio', root_dir=APP_WORKSPACE_PATH)
+    MODBUS = Logger(log_name='modbus', root_dir=APP_WORKSPACE_PATH)
+    SHELLY = Logger(log_name='shelly', root_dir=APP_WORKSPACE_PATH)
+    EVSE = Logger(log_name='evse', root_dir=APP_WORKSPACE_PATH)
+    OPT = Logger(log_name='optimize', root_dir=APP_WORKSPACE_PATH)
+    GUI = Logger(log_name='gui_port', root_dir=APP_WORKSPACE_PATH)
+    CSVLoader = Logger(log_name='CSV_loader', root_dir=APP_WORKSPACE_PATH)
+    GROUP = LoggerGroup(root_dir=APP_WORKSPACE_PATH)
 
 
-for log in [Log.RAS, Log.CSMS, Log.WS, Log.CP, Log.OCPP, Log.WEB, Log.GPIO, Log.MODBUS, Log.SHELLY, Log.EVSE, Log.OPT, Log.GUI, Log.CSVLoader]:
+for log in Log:
+    if isinstance(log, LoggerGroup):
+        log.set_file_count_limit(30)
+        log.set_file_size_limit_kB(100*1000)
+        continue
+    log: Logger
     log.set_exclude_funcs(['_send_signal_info', '__send_signal_recv', '__send_signal_info', '__send_signal', '_log', 'log', 'who_called_me', 'send_web_error_message', '_send'])
     log.set_exclude_classes(['XSignal', 'EventSignal'])
-    log.set_exclude_modules(['server', 'serving'])
+    log.set_exclude_modules(['server', 'serving', 'WebSocket_Client'])
     log.set_level(LogLevel.INFO)
     log.set_file_count_limit(30)
     log.set_file_size_limit_kB(20*1000)
     log.set_highlight_type(LogHighlightType.HTML)
+    log.set_enable_runtime_zip(True)
