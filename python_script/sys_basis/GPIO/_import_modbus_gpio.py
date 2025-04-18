e5d1fa0d26f9b1f95a2fce9657abfaac0d1b3e85
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-04-18 16:14:25 +0200

    Update _import_modbus_gpio.py

--- python_script/sys_basis/GPIO/_import_modbus_gpio.py
+++ python_script/sys_basis/GPIO/_import_modbus_gpio.py
@@ -1,11 +1,11 @@
 
 # **************************** [实际用] ****************************.
 
-# from pymodbus.pdu.pdu import ModbusPDU
-# from pymodbus.client import ModbusSerialClient
-# from gpiozero import Button, LED
+from pymodbus.pdu.pdu import ModbusPDU, ExceptionResponse
+from pymodbus.client import ModbusSerialClient
+from gpiozero import Button, LED
 
 # **************************** [测试用] ****************************
 
-from ._test_Module import Button, LED
-from ._test_Module import ModbusPDU, ModbusSerialClient
+# from ._test_Module import Button, LED
+# from ._test_Module import ModbusPDU, ModbusSerialClient, ExceptionResponse
