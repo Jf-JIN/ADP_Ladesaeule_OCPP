fca9f64f0d1c60db37e144a25eed654d27b28112
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-04-23 12:28:28 +0200

    Create Shelly_Data_CVS_Writer.py

---
+++ python_script/sys_basis/Shelly_Data_CVS_Writer.py
@@ -0,0 +1,33 @@
+
+
+import csv
+
+
+class ShellyDataCVSWriter:
+    def __init__(self, cu_id: int):
+        self.__cu_id = cu_id
+        self.__root_path = ''
+        self.__start_time = '00-00-00T00:00:00Z'
+        self.__csv_file_path = f"ID{self.__cu_id}_{self.__start_time}data.csv"
+
+    @property
+    def cvs_file_path_list(self):
+        ...
+
+    def write_shelly_data(self, data: list):
+        ...
+
+    def stop_writing(self):
+        ...
+
+    def start_writing(self):
+        ...
+
+    def set_current_action(self, plan: dict, value_unit='W'):
+        ...
+
+    def __set_enable_charge(self, enable: bool):
+        ...
+
+    def __set_start_charging_time(self, start_time: str):
+        self.__start_time = start_time
