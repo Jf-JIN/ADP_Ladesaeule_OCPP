ad9de2ac9cff6e645488a296dfaf0ff9ccf64f49
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-04-28 13:59:58 +0200

    Update Charge_Point_Parameters.py

--- python_script/const/Charge_Point_Parameters.py
+++ python_script/const/Charge_Point_Parameters.py
@@ -1,5 +1,6 @@
 
 from const.Analog_Define import AnalogDefine
+from tools.check_func import import_data
 
 
 class _Response(AnalogDefine):
@@ -14,6 +15,9 @@ class _Response_Result(AnalogDefine):
     ERROR = 2
 
 
+_dct = import_data()
+
+
 class CP_Params(AnalogDefine):
     RESPONSE = _Response()
     RESPONSE_RESULT = _Response_Result()
@@ -25,9 +29,9 @@ class CP_Params(AnalogDefine):
     RESPONSE_TIMEOUT = 30  # 响应超时时间, 作用于充电桩
     OCPP_LISTEN_INTERVAL = 1  # OCPP监听间隔时间
     DO_SEND_DEFAULT_RESPONSE = False  # 是否在规定的响应时间内发送默认响应
-    HOST = 'ws://130.83.148.29'  # 优化算法器OCPP服务器地址
+    HOST = 'ws://130.83.148.29' if 'ocpp_opt_host' not in _dct else _dct['ocpp_opt_host']  # 优化算法器OCPP服务器地址
     # HOST = 'ws://172.18.54.149'  # 优化算法器OCPP服务器地址
-    PORT = 80
+    PORT = 80 if 'ocpp_opt_port' not in _dct else _dct['ocpp_opt_port']
     # url = 'ws://192.168.43.65:1234'
     # url = 'ws://172.12.26.71:1234'
     # url = 'ws://130.83.148.29:80'
